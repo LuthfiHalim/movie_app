696ee2770a7179bc70eb0794c6d1d876d33bea48
Luthfi Halim <lhluthfihalim@gmail.com>
2019-12-11 12:41:48 +0700

    update file

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_app/movie.dart';
 import 'package:movie_app/pagesroute.dart';
@@ -8,6 +9,9 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
   List<Movie> popularMovies;
   List<Movie> nowPlayingMovies;
   List<Movie> topRatedMovies;
@@ -33,6 +37,37 @@ class _HomeScreenState extends State<HomeScreen> {
   void initState() {
     super.initState();
     getMovies();
+
+  _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+        //_showItemDialog(message);
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+        //_navigateToItemDetail(message);
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+        //_navigateToItemDetail(message);
+      },
+    );
+    // _firebaseMessaging.requestNotificationPermissions(
+    //     const IosNotificationSettings(sound: true, badge: true, alert: true));
+    // _firebaseMessaging.onIosSettingsRegistered
+    //     .listen((IosNotificationSettings settings) {
+    //   print("Settings registered: $settings");
+    // });
+    _firebaseMessaging.getToken().then((String token) {
+      assert(token != null);
+      setState(() {
+      });
+      print(token);
+
+    });
+
+
+
   }
 
   @override
